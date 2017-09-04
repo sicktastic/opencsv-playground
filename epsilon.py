9621377fd257efb620749535ffc552880c30a393
Anthony Lee <anthonylee@gatech.edu>
2017-09-03 22:10:31 -0500

    Add goals

--- epsilon.py
+++ epsilon.py
@@ -9,6 +9,25 @@ img = cv2.imread('1_opencv.jpg', 0)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
+# Display with Matplotlib
 plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
+
+# 1. Change the image apeture +1
+    # a. Export to JPG
+    # b. Export to PNG
+
+#2. Change the image apeture +2
+    # a. Export to JPG
+    # b. Export to PNG
+
+#3. Change the image apeture -1
+    # a. Export to JPG
+    # b. Export to PNG
+
+#4. Change the image apeture -2
+    # a. Export to JPG
+    # b. Export to PNG
+
+

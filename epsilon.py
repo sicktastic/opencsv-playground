97489ad5c3271f25f4e58510f7c20a0d097ee221
Anthony Lee <anthonylee@gatech.edu>
2017-09-03 21:55:08 -0500

    Add open image in matplotlib

--- epsilon.py
+++ epsilon.py
@@ -1,8 +1,14 @@
-import numpy
+import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 # load in gray scale
 img = cv2.imread('1_opencv.jpg', 0)
-cv2.imshow('1_opencv.jpg', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+# cv2.imshow('1_opencv.jpg', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()

cd2d5218b9afe153222a8697c817300d5a5d34a2
Anthony Lee <antwonlee@gmail.com>
2017-09-22 11:50:54 -0500

    Add basic matrices

--- basics/image_to_matrix.py
+++ basics/image_to_matrix.py
@@ -1,9 +1,14 @@
 import numpy as np
 import cv2
 
-IMAGE = cv2.imread('./sample_images/anthonylee.jpg', 1)
+IMAGE = cv2.imread('sample_images/anthonylee.jpg', 1)
+
+# Show image
+# cv2.imshow('sample_images/anthonylee.jpg', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 image_to_matrix = np.float64(IMAGE)
-
-print(image_to_matrix)
+# print(image_to_matrix)
+# print(image_to_matrix[range(1, 10)])
 print("There are", len(image_to_matrix), "matrices here.")

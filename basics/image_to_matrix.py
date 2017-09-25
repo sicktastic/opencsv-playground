fe8cbdfe1cdba1d205a2cd15743122e38b78ce95
Anthony Lee <antwonlee@gmail.com>
2017-09-25 11:09:35 -0500

    Add numpy reviews
    Adding more numpy review

--- basics/image_to_matrix.py
+++ basics/image_to_matrix.py
@@ -3,12 +3,13 @@ import scipy as sp
 import scipy.signal
 import cv2
 
-def generating_kernel(a):
-  w_1d = np.array([0.25 - a/2.0, 0.25, a, 0.25, 0.25 - a/2.0])
-  return np.outer(w_1d, w_1d)
+# def generating_kernel(a):
+#   w_1d = np.array([0.25 - a/2.0, 0.25, a, 0.25, 0.25 - a/2.0])
+#   return np.outer(w_1d, w_1d)
 
 
-outimage = scipy.signal.convolve2d(kernel,kernel,'same')
+# outimage = scipy.signal.convolve2d(kernel,kernel,'same')
+
 # ------------------------------------------------------------------
 #
 #
@@ -22,10 +23,30 @@ outimage = scipy.signal.convolve2d(kernel,kernel,'same')
 #                        0000              000000              fedc
 #                                          0E0F00
 
+arr = np.array([[1, 2, 1, 10, 20], [3, 4, 3, 30, 40], [5, 6, 5, 50, 60], [7, 8, 7, 70, 80], [9, 10, 9, 90, 100]])
 
-arr = np.array(["A", "B"] ["C, "D"])
 
-print(arr)
+arr_shape = arr.shape
+x = arr_shape[0]
+y = arr_shape[1]
+expand_arr_with_zeros = np.zeros([x * 2, y * 2])
+
+expand_arr_with_zeros[::2, ::2] = arr[:, :]
+
+a_array = []
+a_array.append(arr)
+print(a_array)
+
+b_array = [arr]
+print(b_array)
+
+for i in range (0, 10):
+    print("Hello")
+
+# print(arr)
+# print("---------------")
+# print(expand_arr_with_zeros)
+
 
 # ------------------------------------------------------------------
 
---
+++ basics/numpy_review.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+// linespace, zero, ones, datatypes
+
+print(np.linspace[5, 15, 9])

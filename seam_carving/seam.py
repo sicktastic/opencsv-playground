bfaacb9e0fff0859a98ca60e3f462467b013d39d
Anthony Lee <antwonlee@gmail.com>
2017-10-03 23:11:57 -0500

    Add sobel

--- seam_carving/seam.py
+++ seam_carving/seam.py
@@ -3,4 +3,10 @@ import cv2
 
 img =  cv2.imread('./images/fig5.png', 0)
 
-print(img.shape)
+sobel_x = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+sobel_y = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+
+print(np.float64(img[0]))
+print("==========================")
+print(sobel_x[0])
+

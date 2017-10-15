fcedf21c29cbde4ee65ce7f9b86b06ad7c977b9a
Anthony Lee <antwonlee@gmail.com>
2017-10-15 13:19:35 -0500

    Fix typos

--- dynamic_programing/fibonacci.py
+++ dynamic_programing/fibonacci.py
@@ -34,9 +34,9 @@ print(fib2(15))
 # Bottom up approach
 
 # fib = {}
-# for k in range(k):
+# for k in range(1, n+1):
 #     if k <= 2: f = 1
-#     else: f = fib(n-1)+fib(n+2)
+#     else: f = fib[k-1]+fib[k+2]
 #     fib[k] = f
 # return fib[n]
 

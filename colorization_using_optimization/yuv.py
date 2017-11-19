be7da25fd9a9a9b1137eba4eda569e65187fb5e1
Anthony Lee <antwonlee@gmail.com>
2017-11-18 19:44:09 -0600

    Delete spaces

--- colorization_using_optimization/yuv.py
+++ colorization_using_optimization/yuv.py
@@ -7,9 +7,7 @@ import numpy as np
 
 img =  cv2.imread('./images/anthonylee.jpg')
 b,g,r = cv2.split(img)
-
 img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-
 img_rgb = cv2.merge([r,g,b])
 
 print("1. BRG  --------------------")

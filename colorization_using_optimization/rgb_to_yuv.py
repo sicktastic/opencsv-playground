b04d06829569c6f24456d62c40201e7dd4d4887d
Anthony Lee <antwonlee@gmail.com>
2017-11-25 15:23:08 -0600

    Add colorization template

---
+++ colorization_using_optimization/rgb_to_yuv.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def rgb_to_yuv(rgb):
+    return yuv
--- colorization_using_optimization/yuv.py
+++ colorization_using_optimization/yuv.py
@@ -5,7 +5,6 @@
 
 import cv2
 import math
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy import sparse
 from scipy.sparse.linalg import spsolve
@@ -32,51 +31,3 @@ print("Image BRG Shape:", img.shape)
 print("Image YUV Shape:", img_yuv.shape)
 print("Image RGB Shape:", img_rgb.shape)
 print("Image Gray Shape:", img_rgb.shape)
-
-# Helper functions to convert from RGB color space to YIQ color space and vice-versa.
-
-# Values for conversion matrices taken from http://en.wikipedia.org/wiki/YIQ#Formulas
-
-def rgb2yiq(rgb):
-  """Convert RGB image to YIQ image.
-  Output values are restricted as follows
-  .. math::
-    Y \in [0,1]\\
-    I \in [-0.5957,0.5957]\\
-    Q \in [-0.5226,0.5226]
-  Args:
-    rgb (array_like): An array of shape (m,n,3) and type float.
-  Returns:
-    array_like: An array of the same shape as the input, colours converted to YIQ.
-
-  """
-  conv = np.array([[0.299,0.595716,0.211456],
-                   [0.587,-0.274453,-0.522591],
-                   [0.114,-0.321263,0.311135]])
-  # convert
-  yiq = np.dot(rgb, conv)
-  # check boundaries
-  y = yiq[:,:,0]
-  i = yiq[:,:,1]
-  q = yiq[:,:,2]
-  y[y < 0] = 0
-  y[y > 1] = 1
-  i[i < -0.5957] = -0.5957
-  i[i > 0.5957] = 0.5957
-  q[q < -0.5226]  = -0.5226
-  q[q > 0.5226] = 0.5226
-  return yiq
-
-# YUV Color Space
-# Y is the monochromatic luminance channel
-# which will refer to simply as intensity,
-# while U and V are the chrominance channels,
-# encoding the color.
-
-# calculate weights of neighbors
-
-# w_rs = e^{-(Y(s)-Y(r))^2 / 2\sigma_r^2}
-
-# where r is the index of the center, s the index of a neighbour and
-
-# sigma_r is the variance in a neighbourhood around r.

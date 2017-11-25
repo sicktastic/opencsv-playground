e2eded24e0267cf76e72ee5dc9b9c0871c352d2b
Anthony Lee <antwonlee@gmail.com>
2017-11-25 15:38:13 -0600

    Delete unused examples

--- colorization_using_optimization/colorize.py
+++ colorization_using_optimization/colorize.py
@@ -1,3 +1,15 @@
 import cv2
-import numpy as np
+import math
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+from scipy.sparse.linalg import lsqr
+
+# import images
+bw =  cv2.imread('./images/bw.bmp')
+marked =  cv2.imread('./images/marked.bmp')
+
+yuv = cv2.cvtColor(bw, cv2.COLOR_BRG2YUV)
+
+print(yuv[0])

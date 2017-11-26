96ed239e5be1b73e814bfd4edf86c7ead8376903
Anthony Lee <antwonlee@gmail.com>
2017-11-25 23:56:13 -0600

    Rename files

--- colorization_using_optimization/colorize.py
+++ colorization_using_optimization/colorize.py
@@ -3,18 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-# import images
-bw =  cv2.imread('./images/bw.bmp')
+# colorization images
+gray =  cv2.imread('./images/gray.bmp')
 marked =  cv2.imread('./images/marked.bmp')
-example = cv2.imread('./images/example.png')
 
-yuv = cv2.cvtColor(marked, cv2.COLOR_BGR2YUV)
+# from matlab code colorize.m
+solver = 2
 
-y = yuv[:,:,0]
-u = yuv[:,:,1]
-v = yuv[:,:,2]
+gray = gray.astype(np.float32) / 255
+marked = marked.astype(np.float32) / 255
 
-# print(y[0,1], u[0,1], v[0,1])
+# yuv = cv2.cvtColor(marked, cv2.COLOR_BGR2YUV)
 
-# from matlab code colorize.m line 23
-max_d = np.floor(np.log(min(yuv.shape[0],yuv.shape[1]))/np.log(2)-2)
+# y, u, v = yuv[:,:,0], yuv[:,:,1], yuv[:,:,2]
+
+# from matlab code colorize.m
+# max_d = np.floor(np.log(min(yuv.shape[0],yuv.shape[1]))/np.log(2)-2)
